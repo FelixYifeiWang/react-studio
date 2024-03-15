da9fafefbfdb07a51d8e1852489697c92831323d
王逸飞 <wangyifei@wangyifeidembp.devices.brown.edu>
2024-03-14 20:04:22 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -11,18 +12,55 @@ bakeryData.forEach((item) => {
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (item) => {
+    setCart(currentCart => {
+      const cartItemIndex = currentCart.findIndex(ci => ci.name === item.name);
+      if (cartItemIndex > -1) {
+        // Item already exists, update the quantity
+        const newCart = [...currentCart];
+        newCart[cartItemIndex] = {
+          ...newCart[cartItemIndex],
+          quantity: newCart[cartItemIndex].quantity + 1
+        };
+        return newCart;
+      } else {
+        // Item does not exist, add a new entry
+        return [...currentCart, { ...item, quantity: 1 }];
+      }
+    });
+  };
+
 
   return (
     <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+      <div className="left-container">
+        <h1>Felix's Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
-      ))}
-
-      <div>
-        <h2>Cart</h2>
+        <div className="bakery-items-container">
+          {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
+            <BakeryItem name = {item.name} description = {item.description} price = {item.price} image = {item.image} onAddToCart={() => addToCart(item)}/>
+          ))}
+        </div>
+      </div>
+      <div className="cart-container">
+        <h2>My Cart</h2>
         {/* TODO: render a list of items in the cart */}
+        {cart.length > 0 ? (
+          <>
+            {cart.map((item, index) => (
+              <div key={index} className="itemNum">
+                {item.quantity} x {item.name}
+              </div>
+            ))}
+            <div className="total">
+              Total: ${cart.reduce((total, item) => total + item.price * item.quantity, 0).toFixed(2)}
+            </div>
+          </>
+        ) : (
+          <div className="nothing">Nothing here just yet!</div>
+        )}
       </div>
     </div>
   );
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,39 @@
-// TODO: create a component that displays a single bakery item
+// // TODO: create a component that displays a single bakery item
+import React from "react";
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box'; // Import Box component for additional control
+
+function BakeryItem(props) {
+    return (
+        <Card sx={{ maxWidth: 400, minWidth: 400, display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+            <CardMedia
+                component="img"
+                height="200"
+                image={props.image}
+                alt={props.name}
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    {props.name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    {props.description}
+                </Typography>
+            </CardContent>
+            <CardActions sx={{ justifyContent: 'space-between', paddingRight: 2 , marginLeft: 1.5, marginBottom: 1}}>
+                <Typography variant="body1" color="text.primary">
+                    ${props.price}
+                </Typography>
+                <Button size="medium" onClick={props.onAddToCart}>Add to Cart</Button>
+            </CardActions>
+        </Card>
+    );
+}
+
+export default BakeryItem;
+
